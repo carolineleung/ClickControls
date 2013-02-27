230d9d0f996cb0a3106eaf5b9d817482eec4cd08
Caroline Leung <caroline.leung@gmail.com>
2013-02-26 23:46:16 -0800

    Implemented toggle control with a simple image view

--- src/com/carolineleung/clickcontrols/ClickControlsWidgetProvider.java
+++ src/com/carolineleung/clickcontrols/ClickControlsWidgetProvider.java
@@ -1,75 +1,65 @@
 package com.carolineleung.clickcontrols;
 
-import java.util.Random;
-
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.wifi.WifiManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.RemoteViews;
 
 public class ClickControlsWidgetProvider extends AppWidgetProvider {
-
-	private static final String TAG = "ClickControls";
-	private static final String WIFI_ENABLED = "wifi_enabled";
 
 	public static final String EXTRA_CONTROL = "com.carolineleung.clickcontrols.TOGGLE";
 
 	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		ComponentName thisWidget = new ComponentName(context, ClickControlsWidgetProvider.class);
-		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-
-		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-
-		// if (wifiEnabled) {
-		// remoteViews.setImageViewResource(R.id.btnRecordToggle, R.drawable.record_button_enabled);
-		// } else {
-		// remoteViews.setImageViewResource(R.id.btnRecordToggle, R.drawable.record_button);
-		// }
-		// ComponentName componentName = new ComponentName(context, ClickControlsWidget.class);
-		// AppWidgetManager.getInstance(context).updateAppWidget(componentName, remoteViews);
-
-		for (int widgetId : allWidgetIds) {
-
-			// TODO fix below when test on phone
-			// boolean wifiEnabled = wifiManager.isWifiEnabled();
-			SharedPreferences prefs = context.getSharedPreferences(TAG, Context.MODE_PRIVATE);
-			boolean wifiEnabled = prefs.getBoolean(WIFI_ENABLED, false);
-			Log.d(TAG, "Toggle State: " + wifiEnabled);
-
-			int number = (new Random().nextInt(100));
-
-			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-			wifiEnabled = !wifiEnabled;
-			wifiManager.setWifiEnabled(wifiEnabled);
-			String wifiEnabledText = wifiEnabled ? "on" : "off";
-			Log.i("ClickControlsWidget", "wifi is " + wifiEnabledText + "_" + String.valueOf(number));
-
-			remoteViews.setTextViewText(R.id.indicatorText, wifiEnabledText + "_" + String.valueOf(number));
-			remoteViews.setViewVisibility(R.id.toggleImage, wifiEnabled ? View.INVISIBLE : View.VISIBLE);
-			remoteViews.setViewVisibility(R.id.indicatorText, wifiEnabled ? View.VISIBLE : View.INVISIBLE);
-
-			SharedPreferences.Editor editor = prefs.edit();
-			editor.putBoolean(WIFI_ENABLED, wifiEnabled);
-			editor.commit();
-
-			// Register an onClickListener
-			Intent intent = new Intent(context, ClickControlsWidgetProvider.class);
-			intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-
-			PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-			remoteViews.setOnClickPendingIntent(R.id.control, pendingIntent);
-			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction() == null) {
+			context.startService(new Intent(context, ClickControlsWidgetService.class));
+		} else {
+			super.onReceive(context, intent);
 		}
 	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		context.startService(new Intent(context, ClickControlsWidgetService.class));
+	}
+
+	// @Override
+	// public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	// ComponentName thisWidget = new ComponentName(context, ClickControlsWidgetProvider.class);
+	// int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+	//
+	// WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+	//
+	// for (int widgetId : allWidgetIds) {
+	//
+	// // boolean wifiEnabled = wifiManager.isWifiEnabled();
+	// SharedPreferences prefs = context.getSharedPreferences(TAG, Context.MODE_PRIVATE);
+	// boolean wifiEnabled = prefs.getBoolean(WIFI_ENABLED, false);
+	// Log.d(TAG, "Toggle State: " + wifiEnabled);
+	//
+	// int number = (new Random().nextInt(100));
+	//
+	// RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+	// wifiEnabled = !wifiEnabled;
+	// wifiManager.setWifiEnabled(wifiEnabled);
+	// String wifiEnabledText = wifiEnabled ? "on" : "off";
+	// Log.i("ClickControlsWidget", "wifi is " + wifiEnabledText + "_" + String.valueOf(number));
+	// remoteViews.setViewVisibility(R.id.toggleImage, wifiEnabled ? View.INVISIBLE : View.VISIBLE);
+	//
+	// SharedPreferences.Editor editor = prefs.edit();
+	// editor.putBoolean(WIFI_ENABLED, wifiEnabled);
+	// editor.commit();
+	//
+	// // Register an onClickListener
+	// Intent intent = new Intent(context, ClickControlsWidgetProvider.class);
+	// intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+	// intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+	//
+	// PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+	// remoteViews.setOnClickPendingIntent(R.id.control, pendingIntent);
+	// appWidgetManager.updateAppWidget(widgetId, remoteViews);
+	// }
+	// }
 
 	// @Override
 	// public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
--- src/com/carolineleung/clickcontrols/ClickControlsWidgetService.java
+++ src/com/carolineleung/clickcontrols/ClickControlsWidgetService.java
@@ -1,62 +1,83 @@
 package com.carolineleung.clickcontrols;
 
-import java.util.Random;
-
+import android.app.Activity;
+import android.app.IntentService;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.IBinder;
-import android.util.Log;
 import android.widget.RemoteViews;
 
-public class ClickControlsWidgetService extends Service {
+public class ClickControlsWidgetService extends IntentService {
 
-	private static final String LOG = "com.carolineleung.clickcontrols";
-
-	@Override
-	public void onStart(Intent intent, int startId) {
-		Log.i(LOG, "ClickControlsWidgetProvider called");
-
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
-
-		int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-
-		ComponentName thisWidget = new ComponentName(getApplicationContext(), ClickControlsWidgetProvider.class);
-		int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
-		Log.w(LOG, "From Intent" + String.valueOf(allWidgetIds.length));
-		Log.w(LOG, "Direct" + String.valueOf(allWidgetIds2.length));
-
-		for (int widgetId : allWidgetIds) {
-			// Create some random data
-			int number = (new Random().nextInt(100));
-
-			RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_layout);
-			Log.w("WidgetExample", String.valueOf(number));
-			// Set the text
-			remoteViews.setTextViewText(R.id.indicatorText, "up: " + String.valueOf(number));
-			// remoteViews.setViewVisibility(R.id.imageButton, );
-
-			// Register an onClickListener
-			Intent clickIntent = new Intent(this.getApplicationContext(), ClickControlsWidgetProvider.class);
-
-			clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-			clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
-
-			PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-			remoteViews.setOnClickPendingIntent(R.id.control, pendingIntent);
-
-			appWidgetManager.updateAppWidget(widgetId, remoteViews);
-		}
-
-		stopSelf();
-
-		super.onStart(intent, startId);
+	public ClickControlsWidgetService() {
+		super("ClickControlsWidgetService");
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
+
+	protected void onHandleIntent(Intent intent) {
+		ComponentName me = new ComponentName(this, ClickControlsWidgetProvider.class);
+		AppWidgetManager mgr = AppWidgetManager.getInstance(this);
+		mgr.updateAppWidget(me, buildUpdate(this));
+	}
+
+	private RemoteViews buildUpdate(Context context) {
+		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+		AudioManager audioManager = (AudioManager) context.getSystemService(Activity.AUDIO_SERVICE);
+		if (audioManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT) {
+			updateViews.setImageViewResource(R.id.toggleButton, R.drawable.toggle_on);
+			audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+		} else {
+			updateViews.setImageViewResource(R.id.toggleButton, R.drawable.toggle_off);
+			audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+		}
+
+		Intent intent = new Intent(this, ClickControlsWidgetProvider.class);
+		PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+		updateViews.setOnClickPendingIntent(R.id.toggleButton, pi);
+
+		return updateViews;
+	}
+
+	// @Override
+	// public void onStart(Intent intent, int startId) {
+	// Log.i(LOG, "ClickControlsWidgetProvider called");
+	//
+	// AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
+	//
+	// int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+	//
+	// ComponentName thisWidget = new ComponentName(getApplicationContext(), ClickControlsWidgetProvider.class);
+	// int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
+	// Log.w(LOG, "From Intent" + String.valueOf(allWidgetIds.length));
+	// Log.w(LOG, "Direct" + String.valueOf(allWidgetIds2.length));
+	//
+	// for (int widgetId : allWidgetIds) {
+	// RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_layout);
+	//
+	// // Register an onClickListener
+	// Intent clickIntent = new Intent(this.getApplicationContext(), ClickControlsWidgetProvider.class);
+	//
+	// clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+	// clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+	//
+	// PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+	// remoteViews.setOnClickPendingIntent(R.id.control, pendingIntent);
+	//
+	// appWidgetManager.updateAppWidget(widgetId, remoteViews);
+	// }
+	//
+	// stopSelf();
+	//
+	// super.onStart(intent, startId);
+	// }
+
 }
